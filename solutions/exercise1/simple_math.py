e158df979cf57c392a7d66e87f7bfa8024f81e08
Daniel Sethio <danielquantum2008@gmail.com>
2024-02-22 15:31:12 +0100

    Improve documentation

--- solutions/exercise1/simple_math.py
+++ solutions/exercise1/simple_math.py
@@ -1,3 +1,21 @@
 # square
 def square(x):
+    '''
+    A simple script to calculate square of a number
+
+    Parameters
+    ----------
+    x : integer
+        input integer number
+
+    Returns
+    -------
+    x * x : integer
+        Result of square of x
+
+    Examples
+    --------
+    >>> square(2)
+    4
+    '''
     return x * x
